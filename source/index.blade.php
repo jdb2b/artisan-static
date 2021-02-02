cb3b3e571465a0988b21abc0d906ddbe87926e93
jd <jd@pop-os.localdomain>
2021-02-02 12:03:53 -0500

    update index

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,18 +3,12 @@
 @section('content')
     <h1>Consulting for early stage founders since 2013</h1>
 
-    <h3>Customer Highlights:</h3>z
-    * [LeadIQ](https://leadiq.com) $2-10M+ ARR <span class="test-css">$10M Series A in May 2020</span>
-    * [15Five](https://15five.com) $2-10M+ ARR <span class="test-css">$30M Series B in June 2019</span>
-    * [Feathr](https://feathr.co) $0-6M+ ARR <span class="test-css">$11M Series A in January 2020</span>
-    * [BrightFunnel](https://brightfunnel.com) $0-2M+ ARR <span class="test-css">$6M Series A in December 2015</span>
-    * [Sailthru](https://sailthru.com) $2-10M+ ARR <span class="test-css">$20M Series C in December 2013</span>
-
-    <p>Artisan Static is a starter template for building a static Jigsaw blog hosted on Netlify.</p>
-
-    <p>This comes with code highlighting, share buttons, comments, analytics, an RSS feed, a contact form, a CMS and more.</p>
-
-    <p>The HTML, CSS and JavaScript in this template are extremely minimal, which makes the code easy to build on top of or replace completely.</p>
+    <h3>Customer Highlights:</h3>
+    <p>[LeadIQ](https://leadiq.com) $2-10M+ ARR <span class="test-css">$10M Series A in May 2020</span></p>
+    <p>[15Five](https://15five.com) $2-10M+ ARR <span class="test-css">$30M Series B in June 2019</span></p>
+    <p>[Feathr](https://feathr.co) $0-6M+ ARR <span class="test-css">$11M Series A in January 2020</span></p>
+    <p>[BrightFunnel](https://brightfunnel.com) $0-2M+ ARR <span class="test-css">$6M Series A in December 2015</span></p>
+    <p>[Sailthru](https://sailthru.com) $2-10M+ ARR <span class="test-css">$20M Series C in December 2013</span></p>
 
     <a href="https://ko-fi.com/Y8Y07ACJ" target="_blank">
         <img src="https://www.ko-fi.com/img/githubbutton_sm.svg" alt="ko-fi">
