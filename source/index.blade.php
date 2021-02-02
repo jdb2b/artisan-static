dbbc70658e69775ef849e29d5f7cc1d9b83d08c8
jdb2b <jd@pop-os.localdomain>
2021-02-02 16:33:31 -0500

    update index

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,10 +3,10 @@
 @section('content')
     <h1>Consulting for early stage founders since 2014</h1>
 
-    <h3>Customer Highlights:</h3>
-    <p><a href="https://leadiq.com" target="_blank">LeadIQ</a> $2-10M+ ARR <br><span class="test-css"> $10M Series A in May 2020</span></p>
-    <p><a href="https://15five.com" target="_blank">15Five</a> $2-10M+ ARR <br><span class="test-css">$30M Series B in Jun 2019</span></p>
-    <p><a href="https://feathr.co" target="_blank">Feathr</a> $0-6M+ ARR <br><span class="test-css">$11M Series A in Jan 2020</span></p>
+    <h3>Recent Customer Highlights:</h3>
+    <p><a href="https://leadiq.com" target="_blank">LeadIQ</a> $2 to 10M+ in ARR in two years<br><span class="test-css"> $10M Series A in May 2020</span></p>
+    <p><a href="https://15five.com" target="_blank">15Five</a> $2 to 10M+ in ARR in two years<br><span class="test-css">$30M Series B in Jun 2019</span></p>
+    <p><a href="https://feathr.co" target="_blank">Feathr</a> $0 to 6M+ in ARR in two years <br><span class="test-css">$11M Series A in Jan 2020</span></p>
 
     <h3>Other Notable Engagements:</h3>
     <p>True Anthem, Brightfunnel (acquired by Terminus), Hazel (acquired by Rise People), Pathgather (acquired by Degreed), Hickory Training (acquired by Moat), WindsorCircle (acquired by WhatCounts)
@@ -14,19 +14,15 @@
     <h3>Testimonials:</h3>
 
     <p><b>Aleks Levental, CEO Feathr</b><br>
-    "Dealing with SaaSwise has been one of the rare instances where someone manages to exceed lofty expectations. I could write several expansive paragraphs describing all of the components of our company they've influenced and improved (GTM, prospecting, sales enablement, customer success, etc.)."</p>
+    "Dealing with SaaSwise has been one of the rare instances where someone manages to exceed lofty expectations. I could write several expansive paragraphs describing all of the components of our company they've influenced and improved (GTM, prospecting, sales enablement, customer success)."</p>
 
     <p><b>Eric Duffy, CEO Pathgather</b><br>
-    "The approach that SaaSwise takes to building a successful business is quite rare and had a transformative impact on our team's sales and customer success goals. I can't thank them enough for elevating our business in the year prior to our acquisition."</p>
+    "The approach that SaaSwise takes to building a successful business is quite rare and had a transformative impact on our team's sales and customer success goals. I can't thank them enough for elevating our business in the year leading up to our acquisition."</p>
 
     <p><b>David Hassell, CEO 15Five</b><br>
-    "SaaSwise immediately changed our company's trajectory at 15Five. They worked across all of our teams to shift our focus to the highest value accounts and helped us hire key people on our go-to-market team. It's one of the best investments we've made with any partner."</p>
+    "SaaSwise changed our company's trajectory by 10X. They worked across all of our teams to shift our focus to the highest value accounts and helped us hire all the key people on our go-to-market team. It's one of the best investments we've made with any partner."</p>
 
           <a href="https://ko-fi.com/Y8Y07ACJ" target="_blank">
         <img src="https://www.ko-fi.com/img/githubbutton_sm.svg" alt="ko-fi">
     </a>
-
-    <h2 id="test">Asset Compilation Test</h2>
-
-    <p>JavaScript test: <a href="#test" class="test-js">click me</a></p>https://www.loom.com/share/d2831956df954a70a9a8f85170fbd977
 @endsection
