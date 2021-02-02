97d7cb2d3d67e071f3709aec2122de839cfc9b32
jdb2b <jd@pop-os.localdomain>
2021-02-02 16:54:25 -0500

    update master

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -39,9 +39,8 @@
         <footer>
             <small>
                 &copy; <span data-year></span> {{ $page->owner->name }} &nbsp;&bull;&nbsp;
-                <a href="/feed.atom">RSS</a> &nbsp;&bull;&nbsp;
-                Maintained by Ranie Santos &nbsp;&bull;&nbsp;
-                <a href="https://github.com/raniesantos/artisan-static">GitHub repo</a>.
+                Maintained by Jon Dwyer &nbsp;&bull;&nbsp;
+                <a href="https://linkedin/in/jondwyer">LinkedIn profile</a>.
             </small>
         </footer>
     </section>
