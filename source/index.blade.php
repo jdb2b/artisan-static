9662ccdb8fafc172cbd5501363f14448cff6b558
jd <jd@pop-os.localdomain>
2021-02-02 12:19:31 -0500

    update index

--- source/index.blade.php
+++ source/index.blade.php
@@ -4,11 +4,10 @@
     <h1>Consulting for early stage founders since 2013</h1>
 
     <h3>Customer Highlights:</h3>
-    <p>[LeadIQ](https://leadiq.com) $2-10M+ ARR <span class="test-css">$10M Series A in May 2020</span></p>
-    <p>[15Five](https://15five.com) $2-10M+ ARR <span class="test-css">$30M Series B in June 2019</span></p>
-    <p>[Feathr](https://feathr.co) $0-6M+ ARR <span class="test-css">$11M Series A in January 2020</span></p>
-    <p>[BrightFunnel](https://brightfunnel.com) $0-2M+ ARR <span class="test-css">$6M Series A in December 2015</span></p>
-    <p>[Sailthru](https://sailthru.com) $2-10M+ ARR <span class="test-css">$20M Series C in December 2013</span></p>
+    <p><a href="https://leadiq.com" target="_blank">LeadIQ</a> $2-10M+ ARR <span class="test-css">$10M Series A in May 2020</span></p>
+    <p><a href="https://15five.com" target="_blank">15Five</a> $2-10M+ ARR <span class="test-css">$30M Series B in June 2019</span></p>
+    <p><a href="https://feathr.co" target="_blank">Feathr</a> $0-6M+ ARR <span class="test-css">$11M Series A in January 2020</span></p>
+    <p><a href="https://brightfunnel.com" target="_blank">Brightfunnel</a> $0-2M+ ARR <span class="test-css">$6M Series A in December 2015</span></p>
 
     <a href="https://ko-fi.com/Y8Y07ACJ" target="_blank">
         <img src="https://www.ko-fi.com/img/githubbutton_sm.svg" alt="ko-fi">
