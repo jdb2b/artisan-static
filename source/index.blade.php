4ee8b15d1bc33bfc5dbd879fc7c400e56a5a1786
jdb2b <jd@pop-os.localdomain>
2021-02-02 15:58:53 -0500

    update index

--- source/index.blade.php
+++ source/index.blade.php
@@ -9,12 +9,18 @@
     <p><a href="https://feathr.co" target="_blank">Feathr</a> $0-6M+ ARR <br><span class="test-css">$11M Series A in Jan 2020</span></p>
 
     <h3>Other Notable Engagements:</h3>
-    <p>True Anthem, Brightfunnel (acquired by Terminus), Hazel (acquired by Rise), Pathgather (acquired by Degreed), Hickory Training (acquired by Moat), WindsorCircle (acquired by WhatCounts)
+    <p>True Anthem, Brightfunnel (acquired by Terminus), Hazel (acquired by Rise People), Pathgather (acquired by Degreed), Hickory Training (acquired by Moat), WindsorCircle (acquired by WhatCounts)
 
     <h3>Testimonials:</h3>
-    <p>"Dealing with SaaSwise has been one of the rare instances where someone manages to exceed lofty expectations. I could write several expansive paragraphs describing all of the components of our company they've influenced and improved (GTM, prospecting, sales enablement, customer success, etc.)." - Aleks Levental, CEO Feathr</p>
-    <p>"The approach that SaaSwise takes to building a successful business is quite rare and had a transformative impact on our team's sales and customer success goals. I can't thank them enough for elevating our business in the year prior to our acquisition." - Eric Duffy, CEO Pathgather</p>
-    <p>"SaaSwise immediately changed our company's trajectory at 15Five. They worked across all of our teams to shift our focus to the highest value accounts and helped us hire key people on our go-to-market team. It's one of the best investments we've made with any partner." - David Hassell, CEO 15Five</p>
+
+    <p><b>Aleks Levental, CEO Feathr</b><br>
+    "Dealing with SaaSwise has been one of the rare instances where someone manages to exceed lofty expectations. I could write several expansive paragraphs describing all of the components of our company they've influenced and improved (GTM, prospecting, sales enablement, customer success, etc.)."</p>
+
+    <p><b>Eric Duffy, CEO Pathgather</b><br>
+    "The approach that SaaSwise takes to building a successful business is quite rare and had a transformative impact on our team's sales and customer success goals. I can't thank them enough for elevating our business in the year prior to our acquisition."</p>
+
+    <p><b>David Hassell, CEO 15Five</b><br>
+    "SaaSwise immediately changed our company's trajectory at 15Five. They worked across all of our teams to shift our focus to the highest value accounts and helped us hire key people on our go-to-market team. It's one of the best investments we've made with any partner."</p>
 
           <a href="https://ko-fi.com/Y8Y07ACJ" target="_blank">
         <img src="https://www.ko-fi.com/img/githubbutton_sm.svg" alt="ko-fi">
