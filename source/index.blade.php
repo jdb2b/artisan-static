b73944234a9abc70af959c00813d925c9f60768b
jdb2b <jd@pop-os.localdomain>
2021-02-03 07:54:29 -0500

    update index

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,9 +3,9 @@
 @section('content')
     <h1>Consulting for early stage founders since 2014</h1>
 
-    <h3>Services:
-    <p><b>Operations</b> scaling Go to Market teams via analysis, hiring, playbooks, and technology deployment.</p>
-    <p><b>Coaching</b> accelerating revenue with mid-market companies via consultative sales and customer engagement.</p>
+    <h3>Services:</h3>
+    <p><b>Operations</b> - scaling Go to Market teams via analysis, hiring, playbooks, and technology deployment.</p>
+    <p><b>Coaching</b> - accelerating revenue with mid-market companies via consultative sales and customer engagement.</p>
 
     <h3>Recent Customer Highlights:</h3>
     <p><a href="https://leadiq.com" target="_blank"><img src="https://res.cloudinary.com/saaswise/image/upload/v1612304308/liqsw_hweabl.svg" alt="LeadIQ"></a><br>$2 to 10M+ in ARR in two years <span class="test-css"> $10M Series A in May 2020</span></p>
@@ -18,13 +18,13 @@
     <h3>Testimonials:</h3>
 
     <p><b>Aleks Levental, CEO Feathr</b><br>
-    "Dealing with Jon has been one of the rare instances where someone manages to exceed lofty expectations. I could write several expansive paragraphs describing all of the components of our company he's influenced and improved (prospecting, sales enablement, customer success)."</p>
+    "Dealing with Jon has been one of the rare instances where someone manages to exceed lofty expectations. I could write several expansive paragraphs describing all the components of our company he's influenced and improved (prospecting, sales enablement, customer success)."</p>
 
     <p><b>Eric Duffy, CEO Pathgather</b><br>
-    "The approach that Jon takes to building a successful business is quite rare and had a transformative impact on our team's sales and customer success goals. I can't thank him enough for elevating our business in the year leading up to our acquisition."</p>
+    "The approach that Jon takes to scaling a successful business is quite rare and had a transformative impact on our team's sales and customer success goals. I can't thank him enough for elevating our business for the year leading up to our acquisition."</p>
 
     <p><b>David Hassell, CEO 15Five</b><br>
-    "Jon changed our company's trajectory by 10X. He worked across all of our teams to shift our focus to the highest value accounts and helped us hire all the key people on our go-to-market team. It's one of the best investments we've made with any partner."</p>
+    "Jon changed our company's trajectory by 10x. He worked across all of our teams to shift our focus to the highest value accounts and helped us hire all the key go-to-market people. It's one of the best investments we've made with any partner."</p>
 
           <a href="https://ko-fi.com/Y8Y07ACJ" target="_blank">
         <img src="https://www.ko-fi.com/img/githubbutton_sm.svg" alt="ko-fi">
