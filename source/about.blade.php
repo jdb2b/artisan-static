2f29a1af7024084b819e7e3ecc5890f3c747c0ef
jdb2b <jd@pop-os.localdomain>
2021-02-16 08:17:50 -0500

    update master

--- source/about.blade.php
+++ source/about.blade.php
@@ -5,6 +5,8 @@
 @section('content')
     <h1>About</h1>
 
+    <a href="https://linkedin/in/jondwyer" target="_blank"><img src="{{ media('profile_ovfwfy.svg') }}" alt="LinkedIn"></a>
+
     <p>Started as a bond trader for JP Morgan in the early 90s, then built and ran a SaaS company for a dozen years before helping early stage startups sell and succeed with mid-market/enterprise customers. I'm a big fan of culture-driven startups that seek process & technology to accelerate employee and ARR growth. </p>
 
     <h2>Links:</h2>
